9c6229b5ec793f6301ff2fbe4c32a98b472e67dd
stella <estelle.slrd@gmail.com>
2020-11-24 16:53:46 +0100

    added think protec in a proper human way

--- philo_one/sources/phi_routine.c
+++ philo_one/sources/phi_routine.c
@@ -6,7 +6,7 @@
 /*   By: esoulard <esoulard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 11:56:21 by esoulard          #+#    #+#             */
-/*   Updated: 2020/11/24 16:50:45 by esoulard         ###   ########.fr       */
+/*   Updated: 2020/11/24 16:52:49 by esoulard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,8 +86,8 @@ void		*handle_phi(void *phi)
 		else
 		{
 			if (action_msg(tmp, "is sleeping") ||
-				nap_time(phi, forecast(tmp, tmp->t_sleep))
-				is_dead(tmp) || action_msg(tmp, "is thinking"))
+				nap_time(phi, forecast(tmp, tmp->t_sleep)) ||
+				is_dead(&tmp) || action_msg(tmp, "is thinking"))
 				return (NULL);
 			tmp->status = EATS;
 		}
